18eef86782540c10dc2679476f77c57a9aba18a5
Robert O'Shea <robertosheagamedev@gmail.com>
2025-12-30 02:45:16 +0000

    Formatting changes

--- addons/currency/currency.lua
+++ addons/currency/currency.lua
@@ -1,7 +1,7 @@
-_addon.name = 'currency'
-_addon.author = 'Cromakre'
-_addon.version = '1.0.0.0000'
-_addon.command = 'curr'
+_addon.name = "currency"
+_addon.author = "Cromakre"
+_addon.version = "1.0.0.0000"
+_addon.command = "curr"
 
 packets = require("packets")
 
@@ -17,79 +17,79 @@ local packetBRecieved = false
 local currencyCache = {}
 
 local function inject()
-    packets.inject(packets.new("outgoing", outgoingCurrencyMenu1, {}))
-    packets.inject(packets.new("outgoing", outgoingCurrencyMenu2, {}))
+	packets.inject(packets.new("outgoing", outgoingCurrencyMenu1, {}))
+	packets.inject(packets.new("outgoing", outgoingCurrencyMenu2, {}))
 end
 
 local function stripNames(str)
-    local ret = string.lower(str)
-    ret = string.gsub(ret, "[.'-]+", "")
-    ret = string.gsub(ret, " ", "")
-    return ret
+	local ret = string.lower(str)
+	ret = string.gsub(ret, "[.'-]+", "")
+	ret = string.gsub(ret, " ", "")
+	return ret
 end
 
 local function generateSearchTerm(...)
-    local searchTerm = ""
-    for i, v in ipairs(arg) do
-        searchTerm = searchTerm .. tostring(v)
-    end
+	local searchTerm = ""
+	for i, v in ipairs(arg) do
+		searchTerm = searchTerm .. tostring(v)
+	end
 
-    return stripNames(searchTerm)
+	return stripNames(searchTerm)
 end
 
 local function search(searchTerm)
-    local results = {}
-    for name, val in pairs(currencyCache) do
-        local altered = stripNames(name)
-        if string.match(altered, searchTerm) then
-            results[name] = val
-        end
-    end
-    local keys = {}
-    for k in pairs(results) do
-        table.insert(keys, k)
-    end
-    table.sort(keys)
-    for _, val in ipairs(keys) do
-        windower.add_to_chat(4, val .. "   " .. results[val])
-    end
+	local results = {}
+	for name, val in pairs(currencyCache) do
+		local altered = stripNames(name)
+		if string.match(altered, searchTerm) then
+			results[name] = val
+		end
+	end
+	local keys = {}
+	for k in pairs(results) do
+		table.insert(keys, k)
+	end
+	table.sort(keys)
+	for _, val in ipairs(keys) do
+		windower.add_to_chat(4, val .. "   " .. results[val])
+	end
 end
 
 windower.register_event("addon command", function(command, ...)
-    if command ~= nil then
-        command = command:lower()
-        if command == "search" or command == "s" then
-            inject()
-            local searchTerm = generateSearchTerm(table.unpack(arg))
-            packetARecieved = false
-            packetBRecieved = false
-            windower.add_to_chat(4, "Loading currency...")
-            while not (packetARecieved and packetBRecieved) do
-                -- use this so the game doesn't crash
-                coroutine.sleep(0.01)
-            end
-            search(searchTerm)
-        end
-    else
-        print("Currency addon usage:")
-        print("curr search <term> | curr s <term>")
-        print("> searches for currency that matches <term>")
-    end
+	if command ~= nil then
+		command = command:lower()
+		if command == "search" or command == "s" then
+			inject()
+			local searchTerm = generateSearchTerm(table.unpack(arg))
+			packetARecieved = false
+			packetBRecieved = false
+			windower.add_to_chat(4, "Loading currency...")
+			while not (packetARecieved and packetBRecieved) do
+				-- use this so the game doesn't crash
+				coroutine.sleep(0.01)
+			end
+			search(searchTerm)
+		end
+	else
+		print("Currency addon usage:")
+		print("curr search <term> | curr s <term>")
+		print("> searches for currency that matches <term>")
+	end
 end)
 
 windower.register_event("incoming chunk", function(id, original, modified, injected, blocked)
-    if id == incomingCurrencyMenu1 or id == incomingCurrencyMenu2 then
-        local data = packets.parse("incoming", original)
-        for name, val in pairs(data) do
-            if name ~= nil and not string.match(name, "^_") then
-                currencyCache[name] = val
-            end
-        end
-    end
-    if id == incomingCurrencyMenu1 then
-        packetARecieved = true
-    end
-    if id == incomingCurrencyMenu2 then
-        packetBRecieved = true
-    end
+	if id == incomingCurrencyMenu1 or id == incomingCurrencyMenu2 then
+		local data = packets.parse("incoming", original)
+		for name, val in pairs(data) do
+			if name ~= nil and not string.match(name, "^_") then
+				currencyCache[name] = val
+			end
+		end
+	end
+	if id == incomingCurrencyMenu1 then
+		packetARecieved = true
+	end
+	if id == incomingCurrencyMenu2 then
+		packetBRecieved = true
+	end
 end)
